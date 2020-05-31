b9a152a6b6b6b12ad64f47e5fc607b7208178df5
MyNameIsAditya <adityakhanna@utexas.edu>
2020-05-31 15:36:49 -0500

    Created edge detection functionality

--- liveEdgeDetection.py
+++ liveEdgeDetection.py
@@ -2,5 +2,36 @@ import cv2
 import numpy as np
 
 def main():
+	#Capture the video from a camera input and put it in cap
+	#cap contains ret (which indicates if the operation was successful) and frame (a collection of images)
+	cap = cv2.VideoCapture(0)
+	
+	while (True):
+		ret, frame = cap.read()
+		cv2.imshow("Edge Detection", detectEdges(frame))
+		
+		#Exit if Enter key is pressed
+		if (cv2.waitKey(1) == 13):
+			break
+
+	#Closes camera input and windows
+	cap.release()
+	cv2.destroyAllWindows()  
+
+def detectEdges(frame):
+	#Convert to graysclae image
+	grayscaleImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	#Prepare image with a gaussian blur and remove noise
+	blurredImage = cv2.GaussianBlur(grayscaleImage, (5, 5), 0)
+
+	#Detect edges with Canny algorithm
+	edgesDetectedImage = cv2.Canny(blurredImage, 10, 70)
+
+	return edgesDetectedImage
+    
+    # Do an invert binarize the image 
+    #ret, mask = cv2.threshold(canny_edges, 70, 255, cv2.THRESH_BINARY_INV)
+    #return mask
 
 main()
